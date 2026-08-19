7699ac07b2a94045a8606a01a5cbaf965778ae8f
Harbhajan Singh Bhatiya <harbhajansinghbhatiya2004@gmail.com>
2026-08-19 22:20:41 +0530

    Delete Intermediate Python/name.py

--- Intermediate Python/name.py
+++
@@ -1,5 +0,0 @@
-from collections import namedtuple
-Point = namedtuple('Point', 'x,y')
-pt=Point(1, -4)
-print(pt)  #It returns a named tuple with the values of x and y.
-print(pt.x, pt.y)  #It returns the values of x and y in the named tuple.
